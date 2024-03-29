b64392f7b2498b434aad238791e6688c3dc0b50a
Błażej Błażyca <blazycablazej@gmail.com>
2024-03-29 12:40:51 +0100

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button.tsx";
+
+function App() {
+  return (
+    <div className={"bg-foreground"}>
+      <Button>xd</Button>
+    </div>
+  );
+}
+
+export default App;
